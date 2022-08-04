ac485e55641a1a6823909dc14f5be65c156a8712
Carson Gross <carson@bigsky.software>
2022-08-03 19:59:13 -0600

    hook in schedule runs

--- chill-script/src/main/java/chill/script/types/NoSuchChillPropertyException.java
+++ chill-script/src/main/java/chill/script/types/NoSuchChillPropertyException.java
@@ -4,6 +4,10 @@ import chill.script.expressions.Expression;
 
 public class NoSuchChillPropertyException extends RuntimeException {
     public NoSuchChillPropertyException(Expression expr, Object root, String propName, ChillType runTimeType) {
-        super("Could not find property " + propName + " on " + root + " of type " + runTimeType.getDisplayName());
+        super("Could not find property " + propName + " on " + root + " of type " +
+                runTimeType.getDisplayName() + " in\n\n" +
+                expr.getLineSource() + "\n\n" +
+                expr.getSourceLocation() + "\n\n");
+
     }
 }
